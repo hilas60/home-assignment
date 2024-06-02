d9fbe54850b1d8951d371848b1b836e97ad433d9
Hila S <hilas60@gmail.com>
2024-06-02 18:44:11 +0300

    show posts on client - init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import { Header } from "./components";
 import { PostData, UserData } from "./types";
 import { useUsers } from "./hooks/useUsers";
+import { usePosts } from "./hooks/usePosts";
 
 function App() {
 
   const { users, activeUser, switchUser } = useUsers();
-  const [posts, setPosts] = useState<PostData[]>([]);
+  const { posts } = usePosts();
+
   const [isPostEditorOpen, setIsPostEditorOpen] = useState(false);
 
   const openEditor = () => setIsPostEditorOpen(true);
@@ -14,7 +16,9 @@ function App() {
   return (
     <>
       <Header openPostEditor={openEditor} activeUser={activeUser} switchUser={switchUser} />
-      <div className="posts-wrapper"></div>
+      <div className="posts-wrapper">
+        {posts.map(post => <div key={post.id}>{post.date}</div>)}
+      </div>
     </>
   );
 }
